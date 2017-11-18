9c7d4ce4b2b12a67318f4997195e257c277db2a7
Rafael <rafaelxgs@gmail.com>
2017-11-18 21:37:53 -0200

    Revert "#221 Fix a style issue where popups keep shaking when already repositioned"
    This reverts commit 9da609f4405ce0a4ce1878ce03c9ed6e58e96e21.

--- Extension/esgst.js
+++ Extension/esgst.js
@@ -1436,11 +1436,8 @@ Parsedown = (() => {
                 this.scrollable.style.maxHeight = `${innerHeight * 0.9 - (this.popup.offsetHeight - this.scrollable.offsetHeight)}px`;
             }
             if (!esgst.staticPopups) {
-                let newLeft = (innerWidth - this.popup.offsetWidth) / 2;
-                if (Math.abs(newLeft - this.popup.offsetLeft) > 10) {
-                    this.popup.style.left = `${newLeft}px`;
-                    this.popup.style.top = `${(innerHeight - this.popup.offsetHeight) / 2}px`;
-                }
+                this.popup.style.left = `${(innerWidth - this.popup.offsetWidth) / 2}px`;
+                this.popup.style.top = `${(innerHeight - this.popup.offsetHeight) / 2}px`;
             }
         }
     }
