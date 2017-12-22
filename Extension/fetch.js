e0fb2d14cdb2f11febce6abac67be07572d6e541
Rafael <rafaelxgs@gmail.com>
2017-12-22 17:17:10 -0200

    #329 Add a "From: ESGST" header to requests made by ESGST
    Close #329

--- Extension/fetch.js
+++ Extension/fetch.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, callback) => {
-    fetch(request.url, JSON.parse(request.parameters)).then(response => {
+    let parameters = JSON.parse(request.parameters);
+    parameters.headers = new Headers(parameters.headers);
+    fetch(request.url, parameters).then(response => {
         response.text().then(responseText => {
             callback(JSON.stringify({
                 finalUrl: response.url,
