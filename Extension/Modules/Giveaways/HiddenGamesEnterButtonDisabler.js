10bc6e62f402698c40f39069f4c3370df550fffe
Rafael <gsrafael01@gmail.com>
2018-09-15 10:45:20 -0300

    v7.27.2 (Dev.8) Increase priority of Hidden Game's Enter Button Disabler over default buttons (close #923)

--- Extension/Modules/Giveaways/HiddenGamesEnterButtonDisabler.js
+++ Extension/Modules/Giveaways/HiddenGamesEnterButtonDisabler.js
@@ -1,41 +1,45 @@
 _MODULES.push({
-    description: `
-      <ul>
-        <li>Disables the enter button of any giveaway if you have hidden the game on SteamGifts so that you do not accidentaly enter it.</li>
-      </ul>
-    `,
-    id: `hgebd`,
-    load: hgebd,
-    name: `Hidden Game's Enter Button Disabler`,
-    sg: true,
-    sync: `Hidden Games`,
-    type: `giveaways`
-  });
+  description: `
+    <ul>
+      <li>Disables the enter button of any giveaway if you have hidden the game on SteamGifts so that you do not accidentaly enter it.</li>
+    </ul>
+  `,
+  id: `hgebd`,
+  load: hgebd,
+  name: `Hidden Game's Enter Button Disabler`,
+  sg: true,
+  sync: `Hidden Games`,
+  type: `giveaways`
+});
 
-  function hgebd() {
-    if (!esgst.giveawayPath || document.getElementsByClassName(`table--summary`)[0]) return;
-    let hideButton;
-    hideButton = document.getElementsByClassName(`featured__giveaway__hide`)[0];
-    if (esgst.enterGiveawayButton && !hideButton) {
-      let parent = esgst.enterGiveawayButton.parentElement;
-      if (esgst.enterGiveawayButton) {
-        esgst.enterGiveawayButton.remove();
-      }
-      createElements(parent, `afterBegin`, [{
-        attributes: {
-          class: `sidebar__error is-disabled`
-        },
-        type: `div`,
-        children: [{
-          attributes: {
-            class: `fa fa-exclamation-circle`
-          },
-          type: `i`
-        }, {
-          text: ` Hidden Game`,
-          type: `node`
-        }]
-      }]);
-    }
+function hgebd() {
+  if (!esgst.giveawayPath || document.getElementsByClassName(`table--summary`)[0]) {
+    return;
   }
+  const hideButton = document.getElementsByClassName(`featured__giveaway__hide`)[0];
+  if ((esgst.enterGiveawayButton || (esgst.giveawayErrorButton && !esgst.giveawayErrorButton.textContent.match(/Exists\sin\sAccount/))) && !hideButton) {
+    const parent = (esgst.enterGiveawayButton || esgst.giveawayErrorButton).parentElement;
+    if (esgst.enterGiveawayButton) {
+      esgst.enterGiveawayButton.remove();
+    }
+    if (esgst.giveawayErrorButton) {
+      esgst.giveawayErrorButton.remove();
+    }
+    createElements(parent, `afterBegin`, [{
+      attributes: {
+        class: `sidebar__error is-disabled`
+      },
+      type: `div`,
+      children: [{
+        attributes: {
+          class: `fa fa-exclamation-circle`
+        },
+        type: `i`
+      }, {
+        text: ` Hidden Game`,
+        type: `node`
+      }]
+    }]);
+  }
+}
 
