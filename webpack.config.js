ae52eed3a36275d40159f5d74ce77d41c02486f5
insideone <insideone@rambler.ru>
2018-09-25 22:52:06 +0300

    - almost all external resources included in build - production/development build modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/** @var {Object} webpack.BannerPlugin */
+/** @var {Object} webpack.ProvidePlugin */
+/**
+ * @typedef {Object} Environment
+ * @property {boolean} development
+ * @property {boolean} production
+ */
+
 const
   fs = require('fs'),
   path = require('path'),
@@ -6,102 +14,95 @@ const
   webpack = require('webpack'),
   plugins = {
     clean: require('clean-webpack-plugin'),
-    transfer: require('transfer-webpack-plugin')
+    transfer: require('transfer-webpack-plugin'),
+    sizeAnalyzer: require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
+    progressBar: require('progress-bar-webpack-plugin'),
+    banner: webpack.BannerPlugin,
+    provide: webpack.ProvidePlugin
   },
   BUILD_PATHS = {
     EXTENSION: 'Extension/esgst',
     MONKEY: 'ESGST.user'
   },
-  styleLoader = {
-    loader: 'style-loader',
-    options: {
-      singleton: true,
-      insertInto: 'html'
+  loaders = {
+    style: {
+      loader: 'style-loader',
+      options: {
+        singleton: true,
+        insertInto: 'html'
+      }
+    },
+    css: {
+      loader: 'css-loader',
+      options: { minimize: true }
     }
   }
 ;
 
-// noinspection JSUnresolvedVariable -- don't know why
-plugins.banner = webpack.BannerPlugin;
-// noinspection JSUnresolvedVariable
-plugins.provide = webpack.ProvidePlugin;
-
-let cfg = {
-  mode: 'none',
-  context: path.join(__dirname, './src/entry/'),
-  entry: {
-    [BUILD_PATHS.EXTENSION]: ['./extension/index.js'],
-    [BUILD_PATHS.MONKEY]: ['./monkey/index.js']
-  },
-  output: {
-    path: __dirname,
-    filename: "[name].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = /** @param {Environment} env */ env => {
+  let cfg = {
+    mode: env.production ? 'production' : (env.development ? 'development' : 'none'),
+    context: path.join(__dirname, './src/entry/'),
+    entry: {
+      [BUILD_PATHS.EXTENSION]: ['./extension/index.js'],
+      [BUILD_PATHS.MONKEY]: ['./monkey/index.js']
+    },
+    output: {
+      path: __dirname,
+      filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          loaders: [
+            loaders.style,
+            loaders.css
+          ]
         }
-      },
-      {
-        test: /\.css$/,
-        loaders: [
-          styleLoader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.less$/,
-        loaders: [
-          styleLoader,
-          'css-loader',
-          'lesbooasdadsdfsdfsdfdsfds-loader'
-        ]
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          'file-loader?name=Extension/images/[name].[ext]',
-          'image-webpack-loader?bypassOnDebug'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new plugins.clean(['build'], {exclude: '.gitignore'}),
-    new plugins.banner({
-      raw: true,
-      entryOnly: true,
-      banner: context => {
-        let bannerFile = path.join(cfg.context, path.dirname(context.chunk.name), 'banner.js');
-        if (!fs.existsSync(bannerFile)) {
-          return '';
-        }
+      ]
+    },
+    plugins: [
+      new plugins.banner({
+        raw: true,
+        entryOnly: true,
+        banner: context => {
+          let bannerFile = path.join(cfg.context, path.dirname(context.chunk.name), 'banner.js');
+          if (!fs.existsSync(bannerFile)) {
+            return '';
+          }
 
-        return calfinated.process(fs.readFileSync(bannerFile, 'utf8'), {package: packageJson});
-      }
-    }),
-    new plugins.provide({
-      '$': 'jquery',
-      'jQuery': 'jquery',
-      'window.jQuery': 'jquery',
-      'window.$': 'jquery'
-    }),
-  ]
+          return calfinated.process(fs.readFileSync(bannerFile, 'utf8'), {package: packageJson});
+        }
+      }),
+      new plugins.provide({
+        '$': 'jquery',
+        'jQuery': 'jquery',
+        'window.jQuery': 'jquery',
+        'window.$': 'jquery'
+      }),
+      new plugins.progressBar
+    ].concat(env.development ? [
+      new plugins.sizeAnalyzer
+    ] : []),
+    watch: env.development,
+    watchOptions: {
+      ignored: /node_modules/,
+      poll: 1000,
+      aggregateTimeout: 1000
+    },
+    devtool: env.development ? 'source-map' : null
+  };
+
+  return cfg;
 };
-
-module.exports = cfg;
