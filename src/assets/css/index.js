862d07ae26d95ee6bba7d0082736934c03f5484a
insideone <insideone@rambler.ru>
2018-10-03 23:43:40 +0300

    awesome-bootstrap-checkbox back + this.gf_getFilters/this.filters_addContainer is undefined fix

--- src/assets/css/index.js
+++ src/assets/css/index.js
@@ -1,6 +1,6 @@
 // import 'font-awesome/css/font-awesome.min.css';
 
-// import 'awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css';
+import 'awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css';
 
 import 'jQuery-QueryBuilder/dist/css/query-builder.default.min.css';
 import './patches/query-builder.css';
