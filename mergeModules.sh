68ecc204a527f2d878b6ed92368e0df4848bfa7e
Rafael <gsrafael01@gmail.com>
2018-10-03 14:53:18 -0300

    Update

---
+++ mergeModules.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+modules=""
+while read module; do
+  fileName="Extension/Modules/$module.js"
+  modules+=$(cat $fileName)
+done <Extension/ModulesOrder.txt
+file=$(cat Extension/esgst.dev.js)
+regex="(.+?)//\s\[MODULES\](.+?)"
+if [[ $file =~ $regex ]];
+then
+  echo "${BASH_REMATCH[1]}$modules${BASH_REMATCH[2]}" > Extension/esgst.js
+fi
