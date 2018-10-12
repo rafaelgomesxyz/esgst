554956b401d850e7e88ec7cf849b3226ab874a85
Rafael <gsrafael01@gmail.com>
2018-10-12 13:52:27 -0300

    - Add option to change Level Progress Visualizer and Points Visualizer bar colors (close #968)

--- src/modules/General/PointsVisualizer.js
+++ src/modules/General/PointsVisualizer.js
@@ -1,14 +1,14 @@
 import Module from '../../class/Module';
-import {common} from '../Common';
-
-const
-  createElements = common.createElements.bind(common)
-;
 
 class GeneralPointsVisualizer extends Module {
   constructor() {
     super();
     this.info = {
+      colors: {
+        barColor: `Bar Color`,
+        barColorHover: `Bar Color (Hover / Account Page)`,
+        barColorSelected: `Bar Color (Account Page Hover)`
+      },
       description: `
       <ul>
         <li>Displays a green bar in the account button at the header of any page that represents the amount of points that you have.</li>
@@ -33,12 +33,15 @@ class GeneralPointsVisualizer extends Module {
     const progress = parseInt(percentage * 1.86); // 186px is the width of the button
     const firstBar = `${progress}px`;
     const secondBar = `${Math.max(0, progress - 157)}px`; // 157px is the width of the button without the arrow
+    const barColor = this.esgst.pv_barColor;
+    const barColorHover = this.esgst.pv_barColorHover;
+    const barColorSelected = this.esgst.pv_barColorSelected;
     this.esgst.pvStyleArray = [{
       selector: `.esgst-lpv-container`,
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar, #609f60) ${firstBar}, transparent ${firstBar})`,
+          `linear-gradient(to right, ${barColor} ${firstBar}, transparent ${firstBar})`,
           `var(--esgst-lpv-button, linear-gradient(#8a92a1 0px, #757e8f 8px, #4e5666 100%))`
         ]
       }]
@@ -47,7 +50,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-hover, #6dac6d) ${firstBar}, transparent ${firstBar})`,
+          `linear-gradient(to right, ${barColorHover} ${firstBar}, transparent ${firstBar})`,
           `var(--esgst-lpv-button-hover, linear-gradient(#9ba2b0 0px, #8c94a3 8px, #596070 100%))`
         ]
       }]
@@ -56,7 +59,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-hover, #6dac6d) ${secondBar}, transparent ${secondBar})`,
+          `linear-gradient(to right, ${barColorHover} ${secondBar}, transparent ${secondBar})`,
           `var(--esgst-lpv-button-hover, linear-gradient(#9ba2b0 0px, #8c94a3 8px, #596070 100%))`
         ]
       }]
@@ -65,7 +68,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar, #609f60) ${secondBar}, transparent ${secondBar})`,
+          `linear-gradient(to right, ${barColor} ${secondBar}, transparent ${secondBar})`,
           `var(--esgst-lpv-arrow, linear-gradient(#4e525f 0px, #434857 5px, #2b2e3a 100%))`
         ]
       }]
@@ -74,7 +77,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-hover, #6dac6d) ${firstBar}, transparent ${firstBar})`,
+          `linear-gradient(to right, ${barColorHover} ${firstBar}, transparent ${firstBar})`,
           `var(--esgst-lpv-button-selected, linear-gradient(#d0d5de 0px, #c9cdd7 5px, #9097a6 100%))`
         ]
       }]
@@ -83,7 +86,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-hover, #6dac6d) ${secondBar}, transparent ${secondBar})`,
+          `linear-gradient(to right, ${barColorHover} ${secondBar}, transparent ${secondBar})`,
           `var(--esgst-lpv-button-selected, linear-gradient(#d0d5de 0px, #c9cdd7 5px, #9097a6 100%))`
         ]
       }]
@@ -92,7 +95,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-selected, #7ab97a) ${firstBar}, transparent ${firstBar})`,
+          `linear-gradient(to right, ${barColorSelected} ${firstBar}, transparent ${firstBar})`,
           `var(--esgst-lpv-button-selected-hover, linear-gradient(#f0f1f5 0px, #d1d4de 100%))`
         ]
       }]
@@ -101,7 +104,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-selected, #7ab97a) ${secondBar}, transparent ${secondBar})`,
+          `linear-gradient(to right, ${barColorSelected} ${secondBar}, transparent ${secondBar})`,
           `var(--esgst-lpv-button-selected-hover, linear-gradient(#f0f1f5 0px, #d1d4de 100%))`
         ]
       }]
@@ -110,7 +113,7 @@ class GeneralPointsVisualizer extends Module {
       rules: [{
         name: `background-image`,
         values: [
-          `linear-gradient(to right, var(--esgst-lpv-bar-selected, #7ab97a) ${secondBar}, transparent ${secondBar})`,
+          `linear-gradient(to right, ${barColorSelected} ${secondBar}, transparent ${secondBar})`,
           `var(--esgst-lpv-arrow-selected, linear-gradient(#4e525f 0px, #434857 5px, #2b2e3a 100%))`
         ]
       }]
