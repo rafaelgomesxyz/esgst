251b00eaeee0b52d868956639514f76c4de96395
Rafael <gsrafael01@gmail.com>
2018-10-16 18:13:09 -0300

    - Fix a bug that does not highlight copies from pinned giveaways in Giveaway Copy Highlighter (close #983) - Fix dragging system for giveaway pages (close #984)

--- src/modules/Giveaways/GiveawayCopyHighlighter.js
+++ src/modules/Giveaways/GiveawayCopyHighlighter.js
@@ -9,11 +9,34 @@ class GiveawaysGiveawayCopyHighlighter extends Module {
         <li>Highlights the number of copies next a giveaway's game name (in any page) by coloring it as red and changing the font to bold.</li>
       </ul>
     `,
+      featureMap: {
+        giveaway: `highlight`
+      },
       id: `gch`,
       name: `Giveaway Copy Highlighter`,
       sg: true,
       type: `giveaways`
     };
+  }
+
+  highlight(giveaways) {
+    for (const giveaway of giveaways) {
+      if (!giveaway.copiesContainer) {
+        continue;
+      }
+      const {color, bgColor} = this.esgst.gch_colors.filter(colors => giveaway.copies >= parseInt(colors.lower) && giveaway.copies <= parseInt(colors.upper))[0] || {};
+      giveaway.copiesContainer.classList.add(`esgst-bold`);
+      if (!color) {
+        giveaway.copiesContainer.classList.add(`esgst-red`);
+        continue;
+      }
+      giveaway.copiesContainer.style.color = color;
+      if (!bgColor) {
+        continue;
+      }
+      giveaway.copiesContainer.classList.add(`esgst-gch-highlight`);
+      giveaway.copiesContainer.style.backgroundColor = bgColor;
+    }
   }
 }
 
