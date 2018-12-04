6bb2ed9f5f3791bc31c0551d3a3a6972599bc136
Rafael <gsrafael01@gmail.com>
2018-12-04 11:40:16 -0200

    v8.1.5 (Dev.14) Add an option to show a simplified view of Pagination Navigation On Top (close #1048)

--- src/modules/General/PaginationNavigationOnTop.js
+++ src/modules/General/PaginationNavigationOnTop.js
@@ -14,6 +14,13 @@ class GeneralPaginationNavigationOnTop extends Module {
           [`li`, `Moves the pagination navigation of any page to the main page heading of the page.`]
         ]]
       ],
+      features: {
+        pnot_s: {
+          name: `Enable simplified view (will show only the numbers and arrows).`,
+          sg: true,
+          st: true
+        }
+      },
       id: `pnot`,
       load: this.pnot,
       name: `Pagination Navigation On Top`,
@@ -28,6 +35,12 @@ class GeneralPaginationNavigationOnTop extends Module {
 
     if (this.esgst.st) {
       this.esgst.paginationNavigation.classList.add(`page_heading_btn`);
+    }
+    if (this.esgst.pnot_s) {
+      const elements = this.esgst.paginationNavigation.querySelectorAll(`span`);
+      for (const element of elements) {
+        element.textContent = element.textContent.replace(/[A-Za-z]+/g, ``);
+      }
     }
     this.esgst.paginationNavigation.title = getFeatureTooltip(`pnot`);
     this.esgst.mainPageHeading.appendChild(this.esgst.paginationNavigation);
