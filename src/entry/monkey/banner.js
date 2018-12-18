384cd146b091553a65d9139696c7fbcceb13d2e6
gsrafael01 <gsrafael01@gmail.com>
2018-12-18 10:15:33 -0200

    v8.1.6 (Dev.34) Add a new game category: Banned (close #1102)

--- src/entry/monkey/banner.js
+++ src/entry/monkey/banner.js
@@ -28,6 +28,7 @@
 // @connect graph.microsoft.com
 // @connect userstyles.org
 // @connect royalgamer06.ga
+// @connect steam-tracker.com
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_deleteValue
