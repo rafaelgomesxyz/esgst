f3c8df3019868de763416ccc7a2db12ab2c102e6
gsrafael01 <gsrafael01@gmail.com>
2018-12-18 16:59:30 -0200

    v8.1.7 (Dev.46) Fix a bug that does not allow Shortcut Keys to be used with the native SteamGifts comment box (close #1107)

--- src/modules/General/ShortcutKeys.js
+++ src/modules/General/ShortcutKeys.js
@@ -249,7 +249,7 @@ class GeneralShortcutKeys extends Module {
         if (event.target.tagName === `TEXTAREA`) {
           let reply = event.target.closest(`.comment, .reply_form, .esgst-popup`);
           if (reply) {
-            let button = reply.querySelector(`.esgst-button-set >:first-child, .js_submit`);
+            let button = reply.querySelector(`.esgst-button-set >:first-child, .js__submit-form, .js_submit`);
             if (button) {
               button.click();
               event.preventDefault();
