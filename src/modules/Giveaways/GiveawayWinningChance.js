beb5ffe64d296a83f3ae75b70079c1ce1a4e585f
gsrafael01 <gsrafael01@gmail.com>
2018-12-22 23:17:16 -0200

    v8.1.11 (Dev.6) Add a new feature: Visible Invite-Only Giveaways (close #36)

--- src/modules/Giveaways/GiveawayWinningChance.js
+++ src/modules/Giveaways/GiveawayWinningChance.js
@@ -83,7 +83,7 @@ class GiveawaysGiveawayWinningChance extends Module {
   gwc_addChances(giveaways, main, source) {
     giveaways.forEach(giveaway => {
       if (giveaway.sgTools || (main && (this.esgst.createdPath || this.esgst.wonPath || this.esgst.newGiveawayPath || this.esgst.archivePath))) return;
-      if (((giveaway.inviteOnly && ((main && (this.esgst.giveawayPath || this.esgst.enteredPath)) || !main || giveaway.ended)) || !giveaway.inviteOnly) && !giveaway.innerWrap.getElementsByClassName(`esgst-gwc`)[0]) {
+      if (((giveaway.inviteOnly && ((main && (this.esgst.giveawayPath || this.esgst.enteredPath)) || !main || giveaway.ended || giveaway.id)) || !giveaway.inviteOnly) && !giveaway.innerWrap.getElementsByClassName(`esgst-gwc`)[0]) {
         if (giveaway.started) {
           giveaway.gwcContext = createElements(giveaway.panel, (this.esgst.gv && ((main && this.esgst.giveawaysPath) || (source === `gb` && this.esgst.gv_gb) || (source === `ged` && this.esgst.gv_ged) || (source === `ge` && this.esgst.gv_ge))) ? `afterBegin` : `beforeEnd`, [{
             attributes: {
