0e7e8fa9ba3bda1675d180c75ad1cc5939a6f62f
Rafael <gsrafael01@gmail.com>
2019-02-05 15:46:27 -0200

    v8.2.2-dev.5 #1197

--- src/browser.js
+++ src/browser.js
@@ -16,17 +16,19 @@ if (typeof browser !== `undefined`) {
         return new Promise(resolve => {
           _browser.runtime.sendMessage({
             action: `getPackageJson`
-          }, result => {
+          }).then(result => {
             resolve(JSON.parse(result));
           });
         });
       },
-      sendMessage: (obj, callback) => {
-        obj.uuid = `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, utils.createUuid.bind(utils));
-        self.port.emit(obj.action, obj);
-        self.port.on(`${obj.action}_${obj.uuid}_response`, function onResponse(result) {
-          self.port.removeListener(`${obj.action}_${obj.uuid}_response`, `onResponse`);
-          callback(result);
+      sendMessage: obj => {
+        return new Promise(resolve => {
+          obj.uuid = `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, utils.createUuid.bind(utils));
+          self.port.emit(obj.action, obj);
+          self.port.on(`${obj.action}_${obj.uuid}_response`, function onResponse(result) {
+            self.port.removeListener(`${obj.action}_${obj.uuid}_response`, `onResponse`);
+            resolve(result);
+          });
         });
       }
     }
