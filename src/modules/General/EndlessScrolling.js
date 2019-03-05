1bdcbbb6309a762dc4153c3b943dac396ab85eb5
Rafael <gsrafael01@gmail.com>
2019-03-05 08:13:35 -0300

    v8.3.0-dev.39 Filters: fix filtered count (close #1194)

--- src/modules/General/EndlessScrolling.js
+++ src/modules/General/EndlessScrolling.js
@@ -441,15 +441,6 @@ class GeneralEndlessScrolling extends Module {
         this.es_purgeRemovedElements();
         await endless_load(es.mainContext, true, null, currentPage);
         this.es_setRemoveEntry(es.mainContext);
-        if (this.esgst.gf && this.esgst.gf.filteredCount) {
-          this.esgst.modules.filters.filters_updateCount(this.esgst.gf);
-        }
-        if (this.esgst.df && this.esgst.df.filteredCount) {
-          this.esgst.modules.filters.filters_updateCount(this.esgst.df);
-        }
-        if (this.esgst.cf && this.esgst.cf.filteredCount) {
-          this.esgst.modules.filters.filters_updateCount(this.esgst.cf);
-        }
         if (this.esgst.ts && !this.esgst.us) {
           this.esgst.modules.generalTableSorter.ts_sortTables();
         }
@@ -800,15 +791,6 @@ class GeneralEndlessScrolling extends Module {
       },
       type: `i`
     }]);
-    if (this.esgst.gf && this.esgst.gf.filteredCount) {
-      this.esgst.modules.filters.filters_updateCount(this.esgst.gf);
-    }
-    if (this.esgst.df && this.esgst.df.filteredCount) {
-      this.esgst.modules.filters.filters_updateCount(this.esgst.df);
-    }
-    if (this.esgst.cf && this.esgst.cf.filteredCount) {
-      this.esgst.modules.filters.filters_updateCount(this.esgst.cf);
-    }
     if (this.esgst.ts && !this.esgst.us) {
       this.esgst.modules.generalTableSorter.ts_sortTables();
     }
