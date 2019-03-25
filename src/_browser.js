3d7ad7234fa8cd68c559977964b9b5bea770cead
Rafael <gsrafael01@gmail.com>
2019-03-25 11:28:49 -0300

    v8.3.2-dev.4 Internal update

---
+++ src/_browser.js
@@ -0,0 +1,48 @@
+import { utils } from './lib/jsUtils';
+
+let _browser = null;
+
+if (typeof browser !== `undefined`) {
+  _browser = browser;
+} else if (typeof self !== `undefined`) {
+  _browser = {
+    runtime: {
+      onMessage: {
+        addListener: callback => {
+          self.port.on(`esgstMessage`, obj => callback(obj));
+        }
+      },
+      getManifest: () => {
+        return new Promise(resolve => {
+          _browser.runtime.sendMessage({
+            action: `getPackageJson`
+          }).then(result => {
+            resolve(JSON.parse(result));
+          });
+        });
+      },
+      sendMessage: obj => {
+        return new Promise(resolve => {
+          obj.uuid = `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, utils.createUuid.bind(utils));
+          self.port.emit(obj.action, obj);
+          self.port.on(`${obj.action}_${obj.uuid}_response`, function onResponse(result) {
+            self.port.removeListener(`${obj.action}_${obj.uuid}_response`, `onResponse`);
+            resolve(result);
+          });
+        });
+      }
+    }
+  };
+} else {
+  _browser = {
+    runtime: {}
+  };
+}
+
+if (typeof _browser.runtime.getBrowserInfo === `undefined`) {
+  _browser.runtime.getBrowserInfo = () => ({
+    name: `?`
+  });
+}
+
+export { _browser };
