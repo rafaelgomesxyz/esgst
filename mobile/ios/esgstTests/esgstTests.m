e9b03caceda2f8ba1429f567b4777b3924daaf27
rafaelgssa <rafael.gssa@pm.me>
2019-10-11 14:46:54 -0300

    Rename mobile

--- mobile/ios/esgstTests/esgstTests.m
+++ mobile/ios/esgstTests/esgstTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileTests : XCTestCase
+@interface esgstTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation esgstTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
