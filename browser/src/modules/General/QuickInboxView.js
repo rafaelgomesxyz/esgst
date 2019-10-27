37d3db6b6ce94d04520eba0a9a709b6747884032
rafaelgssa <rafael.gssa@pm.me>
2019-10-27 14:24:35 -0300

    Header Refresher: fix bug that does not update counters correctly (close #1496)

--- browser/src/modules/General/QuickInboxView.js
+++ browser/src/modules/General/QuickInboxView.js
@@ -284,7 +284,7 @@ class GeneralQuickInboxView extends Module {
         elements[i].remove();
       }
 
-      await Shared.header.updateCounter('messages', null);
+      await Shared.header.updateCounter('messages', 0);
     });
   }
 }
