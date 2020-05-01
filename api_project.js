94443d9eb38fdbd1efc0f703b3d7eb31e6d577b1
rafaelgssa <rafael.gssa@pm.me>
2020-05-01 10:04:51 -0300

    Update header

--- api_project.js
+++ api_project.js
@@ -7,13 +7,13 @@ define({
   "sampleUrl": "https://rafaelgssa.com/esgst",
   "header": {
     "title": "",
-    "content": "<p>The requests must be made to the following endpoint:</p>\n<pre class=\"prettyprint\">https://rafaelgssa.com/esgst\n</code></pre>\n"
+    "content": "<p>This API is open source. Check the source on <a href=\"https://github.com/rafaelgssa/esgst/tree/master/server\">GitHub</a>.</p>\n<p>It is currently hosted at <a href=\"https://www.umbler.com/us\">Umbler</a> as a Node.js app.</p>\n<p>The requests must be made to the following endpoint:</p>\n<pre class=\"prettyprint\">https://rafaelgssa.com/esgst\n</code></pre>\n"
   },
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-30T18:42:29.642Z",
+    "time": "2020-05-01T13:02:17.518Z",
     "url": "http://apidocjs.com",
     "version": "0.22.0"
   }
